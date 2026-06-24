9f141b01ad4ab231476be06b12014f796da70a02
nthanhtai-005 <nthanhtaii205@gmail.com>
2026-06-24 15:14:10 +0700

    Loại bỏ chuỗi kết nối cứng, sử dụng app config

--- DAL/Providers/DbConnectionManager.cs
+++ DAL/Providers/DbConnectionManager.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Oracle.ManagedDataAccess.Client;
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Configuration; // Thêm thư viện này
 
 namespace DAL.Providers
 {
     public class DbConnectionManager
     {
-        // Chuỗi kết nối mặc định (Quản trị viên)
-        private string _connectionString = "Data Source=localhost:1521/DB_BAOMAT; User Id=ADMIN_BM; Password=Oracle_1234;";
+        // Đọc chuỗi kết nối từ file App.config thông qua thuộc tính Name là "OracleDbContext"
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString;
 
         public OracleConnection GetConnection()
         {
@@ -19,7 +15,7 @@ namespace DAL.Providers
 
         public OracleConnection GetConnectionWithCredentials(string username, string password)
         {
-            // Trả về kết nối với tài khoản user cụ thể để test đăng nhập DB
+            // Trả về kết nối với tài khoản user cụ thể để test đăng nhập DB hoặc lấy quyền
             return new OracleConnection($"Data Source=localhost:1521/DB_BAOMAT; User Id={username}; Password={password};");
         }
     }
